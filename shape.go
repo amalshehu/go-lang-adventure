96069df87a34d4f59267d02bb0b89203d622bd28
amalshehu <amalshehu@gmail.com>
2016-12-10 15:23:53 +0530

    Find the area and perimeter

---
+++ shape.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+import "os"
+// import "strconv"
+
+const Pi = 3.14
+type Circle struct {
+    radius  float64
+}
+
+type Rectangle struct {
+    length, width  int
+}
+
+func main() {
+  input := os.Args[2]
+  func (c Circle) float64 {
+    return Pi*c.radius*c.radius
+}
+
+}
